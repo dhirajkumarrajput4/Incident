c5d05f39cad1293dca48985770467c5ca22c517a
Dhiraj Kumar <dhirajkumarrajput4@gmail.com>
2024-09-10 21:33:54 +0530

    cors issue

--- src/main/java/com/rapifuzz/config/SecurityConfig.java
+++ src/main/java/com/rapifuzz/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.rapifuzz.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,7 +25,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+//                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
                         auth -> auth.requestMatchers("/api/home/**").authenticated()
                                 .requestMatchers("/api/auth/**").permitAll()
